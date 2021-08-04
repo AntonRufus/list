fa1a0fff0d6bff32fc04f985fe1153b200a37d48
AntonRufus <zhyvotovskyi@gmail.com>
2021-08-04 12:44:22 +0300

    another option used

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
-/*
+
 const devConfig = {
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
@@ -16,7 +16,7 @@ const prodConfig = {
 const pool = new Pool(
   process.env.NODE_ENV === "production" ? prodConfig : devConfig
 );
-*/
+/*
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const prodConfig = process.env.DATABASE_URL; // heroku addons
@@ -25,5 +25,5 @@ const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? prodConfig : devConfig,
 });
-
+*/
 module.exports = pool;
